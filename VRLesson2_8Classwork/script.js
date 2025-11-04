ee028980c4e917266fb01720b4e9eb07204c4643
fangBing0824 <fangbingl3@nycstudents.net>
2025-11-04 03:38:59 +0000

    updates

--- VRLesson2_8Classwork/script.js
+++ VRLesson2_8Classwork/script.js
@@ -9,7 +9,10 @@ window.addEventListener("DOMContentLoaded",function() {
 })
 
 function loop(){
-  target.a += target.da;
+  if (target.a>-90){
+    target.a += target.da;
+  }
+  
 
   target.setAttribute("rotation",{x:target.a, y:0, z: 0});
 
