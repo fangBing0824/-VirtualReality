bc921a5e7ab752f7071bd3085aef9be762349ce5
fangBing0824 <fangbingl3@nycstudents.net>
2025-11-04 03:02:18 +0000

    updates

--- VRLesson2_7Classwork/script.js
+++ VRLesson2_7Classwork/script.js
@@ -1,16 +1,24 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene, dudeTemplate;
+let scene, dudeTemplate, dudes=[];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   dudeTemplate = document.querySelector("#dudeTemplate");
 
   //Challenge 4: Create an array of dudes at random locations.
-  
+  for (let i=0; i<20; i++){
+    let x = rnd(-5, 5);
+    let y = rnd(1, 2);
+    let z = rnd(-10, -5);
+    let d = new Dude(x, y, z);
+    dudes.push(d);
+  }
   loop();  
 })
 function loop(){
   //Challenge 5: Make all the dudes jump.
-  
+  for (let d of dudes){
+    d.jump();
+  }
   window.requestAnimationFrame( loop );
 }
