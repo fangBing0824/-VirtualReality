2c7527946d36a4b1b2e693844714db625aed6612
fangBing0824 <fangbingl3@nycstudents.net>
2025-11-24 01:38:35 +0000

    updates

--- VRPractical3_2/script.js
+++ VRPractical3_2/script.js
@@ -1,16 +1,24 @@
 let scene;
 let snowmen = [];
 let ufos = [];
+let snowflakes=[];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene")
 
-  for(let i = 0; i < 5; i++){
-    snowmen.push(new Snowman(rnd(-10, 10), 0, rnd(-10, 10)));
+  for(let i = 0; i < 20; i++){
+    snowmen.push(new Snowman(rnd(-20, 20), 0, rnd(-20, 20)));
   }
 
   for(let i = 0; i < 3; i++){
-    ufos.push(new UFO(rnd(-10, 10), 10, rnd(-10, 10)));
+    ufos.push(new UFO(rnd(-20, 20), 10, rnd(-20, 20)));
+  }
+
+  for(let i = 0; i < 100; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    let snowflake = new Snowflake(x,z);
+    snowflakes.push(snowflake);
   }
      
   loop();
@@ -26,6 +34,10 @@ function loop(){
   for(let i = 0; i < ufos.length; i++){
     ufos[i].update();
     // console.log("ufos[i].y: ", ufos[i].y);
+  }
+
+  for (let flake of snowflakes) {
+    flake.fall();
   }
 
   window.requestAnimationFrame(loop);
