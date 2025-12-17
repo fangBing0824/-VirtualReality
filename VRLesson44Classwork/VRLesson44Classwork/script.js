0b3dd6d0301b9642b867380a2c5a5dd182278879
fangBing0824 <fangbingl3@nycstudents.net>
2025-12-17 17:21:50 +0000

    updates

---
+++ VRLesson44Classwork/VRLesson44Classwork/script.js
@@ -0,0 +1,17 @@
+let scene, boxes = [];
+window.onload = function(){
+  scene = document.querySelector("a-scene");
+  /* Challenge
+     Create 10 random boxes through the world
+  */
+ 
+}
+
+
+function loop(){
+  /* Challenge 
+     Make the boxes blast off
+  */
+  window.requestAnimationFrame( loop )
+  
+}
